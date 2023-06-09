bed33c40aae6b8f7192f122377c44ecfacfff1f0
Matheus Gois <mattheusgoiis@gmail.com>
2023-06-09 20:29:25 -0300

    Making the landing page

---
+++ app/javascript/main.js
@@ -0,0 +1,21 @@
+
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('on');
+    NavMenu.classList.toggle('on');
+});
+
+VanillaTilt.init(document.querySelectorAll(".home-img"), {
+    max: 25,
+    speed: 400,
+    scale: 1.1
+});
+
+let typed = new Typed(".text-first", {
+    strings: ["Organize", "Improve", "Optimize", "Upgrade"],
+    typeSpeed:  100,
+    backSpeed: 50,
+    loop: true
+})
