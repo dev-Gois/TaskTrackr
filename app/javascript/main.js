1f01def9f8f67bc2b6ff07e3e42c7878f7f4c127
Matheus Gois <mattheusgoiis@gmail.com>
2023-06-09 22:04:00 -0300

    Solving some bugs on landing page

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,10 +1,12 @@
 
 const menu = document.querySelector('.menu');
 const NavMenu = document.querySelector('.nav-menu');
+const image = document.querySelector('.rocket');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('on');
     NavMenu.classList.toggle('on');
+
 });
 
 VanillaTilt.init(document.querySelectorAll(".home-img"), {
